62cc9dfa5713adafbcd7f91eb64c5bae370412eb
vignesh kopparapu <82154963+Vigneshk-3@users.noreply.github.com>
2026-07-03 10:49:36 +0530

    Add: duplicate-integer - submission-3

---
+++ Data Structures & Algorithms/duplicate-integer/submission-3.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        HashSet<Integer> hs = new HashSet<>();
+
+        for (int val : nums) {
+            if (hs.contains(val)) return true;
+            hs.add(val);
+        }
+
+        return false;
+    }
+}
